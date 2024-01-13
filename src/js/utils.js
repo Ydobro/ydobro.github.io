f4f300e530ae10e85c29bff9dfc72cc9f7278b1b
YdobroV <ydobrovolskii@mail.ru>
2024-01-13 15:32:31 +0300

    WIP Intro canvas

--- src/js/utils.js
+++ src/js/utils.js
@@ -18,5 +18,18 @@ const preloadFonts = (families = []) => {
   })
 }
 
+/**
+ * Preloads images specified by the CSS selector.
+ *
+ * @param {string} [selector='img'] - CSS selector for target images.
+ * @returns {Promise} - Resolves when all specified images are loaded.
+ */
+const preloadImages = (selector = 'img') => {
+  return new Promise((resolve) => {
+    // The imagesLoaded library is used to ensure all images (including backgrounds) are fully loaded.
+    imagesLoaded(document.querySelectorAll(selector), { background: true }, resolve)
+  })
+}
+
 // Exporting utility functions for use in other modules.
-export { preloadFonts }
+export { preloadImages, preloadFonts }
